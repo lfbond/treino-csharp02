7afb03b466448b74a6f7c7c9866e645a5b37fd5d
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-25 12:54:44 -0300

    Commit Final do projeto guiado

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46be62ac1713c66a9ade20a919878235d2b82968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4242f989279030e430b74a9f5a5b572555e00f00")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
