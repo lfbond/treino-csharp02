d15878f938e816334ba3e7416d079cae577f4f09
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-25 15:32:46 -0300

    Commit merge

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505b6736f25ab561fd25fd0e1b61e262a2997ff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3746a10055f0c09a96fa12837dc50dec034d2ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
