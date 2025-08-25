505b6736f25ab561fd25fd0e1b61e262a2997ff4
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-25 14:13:34 -0300

    Commit projeto final mod. 03

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4242f989279030e430b74a9f5a5b572555e00f00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7afb03b466448b74a6f7c7c9866e645a5b37fd5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
